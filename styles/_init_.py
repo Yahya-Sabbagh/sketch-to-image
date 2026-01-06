949f2a868c90851d14750b3ad194bd5ad9da975c
SABBAGH Yahya <76563475+Yahya-Sabbagh@users.noreply.github.com>
2026-01-06 21:43:34 +0100

    Initialize styles module with presets import

---
+++ styles/_init_.py
@@ -0,0 +1,15 @@
+from .presets import (
+    StylePreset,
+    STYLE_PRESETS,
+    get_style_preset,
+    get_style_names,
+    apply_style_to_prompt
+)
+
+__all__ = [
+    "StylePreset",
+    "STYLE_PRESETS", 
+    "get_style_preset",
+    "get_style_names",
+    "apply_style_to_prompt"
+]
